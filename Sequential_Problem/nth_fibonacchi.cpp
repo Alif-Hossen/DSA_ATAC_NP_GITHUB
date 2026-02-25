9200d07bbf689368e2ab3081bddc818264f823b1
Alif-Hossen <naimuralif1.0@gmail.com>
2026-02-25 17:59:18 -0600

    Fibonacchi Correction

--- Sequential_Problem/nth_fibonacchi.cpp
+++ Sequential_Problem/nth_fibonacchi.cpp
@@ -3,15 +3,20 @@ using namespace std;
 
 int fibonacci(int n) {
 
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+
     int first = 0, second = 1, next;
 
-    for (int i = 0; i < n; i++) {
-        next = first;
+    for (int i = 2; i <= n; i++) {
+        next = first + second;
         first = second;
-        second = next + second;
+        second = next;
     }
 
-    return first;
+    return second;
 }
 
 int main() {
