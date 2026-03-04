0c71896298e986084d614f1f0f86121aed276fb2
Alif-Hossen <naimuralif1.0@gmail.com>
2026-03-03 20:16:45 -0600

    Bitwise Operation ( leftShift/ rightshift )

---
+++ V_7_Miscellaneous_Concept/Bitwise.cpp
@@ -0,0 +1,89 @@
+// BITWISE AND --> 
+
+        // #include <iostream>
+        // using namespace std;
+
+        // int main() {
+
+        //     int a = 4, b = 8;
+        //     cout << ( a & b ) << endl ;
+            
+        //     return 0;
+        // }
+
+
+// BITWISE OR -->
+
+        // #include <iostream>
+        // using namespace std;
+
+        // int main() {
+
+        //     int a = 4, b = 8;
+        //     cout << ( a | b ) << endl;
+
+        //     return 0;
+        // }
+
+
+// XOR --> 
+
+        // #include <iostream>
+        //         using namespace std;
+
+        //         int main() {
+
+        //             int a = 4, b = 8;
+        //             cout << ( a ^ b ) << endl;
+
+        //             return 0;
+        //         }
+
+
+// BITWISE << ( LEFT SHIFT, LEFT SHIFT ) || ( a * 2^b ) -->
+    // ONE --> 
+        // #include <iostream>
+        // using namespace std;
+
+        // int main() { 
+
+        //     cout << ( 10 << 1 ) << endl; // 16
+
+        //     return 0;
+        // }
+
+    // TWO ->
+
+        // #include <iostream>
+        // using namespace std;
+
+        // int main() {
+
+        //     cout << ( 4 << 2 ) << endl;
+
+        //     return 0;
+        // }
+
+
+// BITWISE << ( RIGHT SHIFT, RIGHT SHIFT )  || ( a / 2^b ) -->
+    // ONE --> 
+        // #include <iostream>
+        // using namespace std;
+
+        // int main() {
+
+        //     cout << ( 10 >> 1 ) << endl; // 5
+
+        //     return 0;
+        // }
+
+    // TWO --> 
+        #include <iostream>
+        using namespace std;
+
+        int main() {
+
+            cout << ( 8 >> 2 ) << endl; // 2
+
+            return 0;
+        }
