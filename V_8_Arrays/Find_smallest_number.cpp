8d271225b7fca67024a1d3f8ba47791f3481f0d7
Alif-Hossen <naimuralif1.0@gmail.com>
2026-03-03 21:47:44 -0600

    Largest, smallest number

---
+++ V_8_Arrays/Find_smallest_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main () {
+
+    int nums[] = { 5, 15, 22, 1, -15, 24 };
+    int size = sizeof( nums ) / sizeof( nums[1] );
+
+    // cout << size;
+    int smallestNumber = INT_MAX;
+
+    for ( int i = 0; i < size; i++ ) {
+        // if( nums[i] < smallestNumber ) {
+        //     smallestNumber = nums[i];
+        // }
+
+        // OR 
+
+        smallestNumber = min(nums[i], smallestNumber);
+    }
+
+    cout << "The Smallest Number Is : " << smallestNumber << endl ;
+
+    return 0;
+}
