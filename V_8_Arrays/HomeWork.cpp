2fc2b805974e320d5f04d7c2eae1b85c06bd5879
Alif-Hossen <naimuralif1.0@gmail.com>
2026-03-05 13:27:23 -0600

    Largest Number's Index

--- V_8_Arrays/HomeWork.cpp
+++ V_8_Arrays/HomeWork.cpp
@@ -1,32 +1,68 @@
 // FIND INDEX OF SMALLEST NUMBER.
 
+            // #include <iostream>
+            // using namespace std;
+
+            // int smallestIndex(int arr[], int size) {
+
+            //     int minIndex = 0;
+
+            //     for (int i = 1; i < size; i++) {
+            //         if (arr[i] < arr[minIndex]) {
+            //             minIndex = i;
+            //         }
+            //     }
+
+            //     return minIndex;
+            // }
+
+            // int main() {
+
+            //     int arr[] = {5, 9, 2, 8, 1, 7, 3};
+            //     int size = 7;
+
+            //     cout << "Smallest Index: " << smallestIndex(arr, size);
+
+            //     return 0;
+            // }
+
+// FIND INDEX OF LARGEST NUMBER.
+
 #include <iostream>
 using namespace std;
 
-int smallestIndex(int arr[], int size) {
-
-    int minIndex = 0;
-
-    for (int i = 1; i < size; i++) {
-        if (arr[i] < arr[minIndex]) {
-            minIndex = i;
+int indexLargestNumber( int arr[], int sz ) {
+    int maximumIndex = 0;
+    for( int i =0; i< sz; i++ ) {
+        if( arr[i] > arr[maximumIndex] ) {
+            maximumIndex = i;
         }
     }
-
-    return minIndex;
+    return maximumIndex;
 }
 
 int main() {
 
-    int arr[] = {5, 9, 2, 8, 1, 7, 3};
-    int size = 7;
+    int size;
+    cout << "Enter Array Size : ";
+    cin >> size;
+    int array[size];
 
-    cout << "Smallest Index: " << smallestIndex(arr, size);
+    for ( int i= 0; i< size; i++ ) {
+        cin >> array[i];
+    }
+
+    cout << "The Array Is : " << endl;
+    for( int i= 0; i< size; i++) {
+        cout << array[i] << " " << endl;
+    }
+
+    int index = indexLargestNumber( array, size );
+    cout << "The Maximum Number Is : " << array[index] << endl;
+    cout << "The Maximum Number's Index Is : " << index << endl;
 
     return 0;
 }
-
-// FIND INDEX OF LARGEST NUMBER.
 
 
 // WAF TO CALCULATE SUM & PRODUCT OFF ALL NUMBERS IN AN ARRAY.
