a07fccd2987a4864316fd8e0d5288890fa368b9f
Alif-Hossen <naimuralif1.0@gmail.com>
2026-03-05 10:30:26 -0600

    Smallest Index

--- V_8_Arrays/HomeWork.cpp
+++ V_8_Arrays/HomeWork.cpp
@@ -1,5 +1,30 @@
 // FIND INDEX OF SMALLEST NUMBER.
 
+#include <iostream>
+using namespace std;
+
+int smallestIndex(int arr[], int size) {
+
+    int minIndex = 0;
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+}
+
+int main() {
+
+    int arr[] = {5, 9, 2, 8, 1, 7, 3};
+    int size = 7;
+
+    cout << "Smallest Index: " << smallestIndex(arr, size);
+
+    return 0;
+}
 
 // FIND INDEX OF LARGEST NUMBER.
 
