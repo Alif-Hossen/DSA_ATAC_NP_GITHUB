f777f3dcb2cb5d2c52c3bbfad794d5681543bad4
Alif-Hossen <naimuralif1.0@gmail.com>
2026-03-06 08:28:20 -0600

    Max Sum Of Sub Array Using Kadane's Algo

--- v _10_Kadane's_Algo_MaxSubArr/Maximum_Subarray_Sum.cpp
+++ v _10_Kadane's_Algo_MaxSubArr/Maximum_Subarray_Sum.cpp
@@ -7,27 +7,66 @@
 
 // BRUTE FORCE ALGORITHM ---->
 
-#include <iostream>
-#include <vector>
+        // #include <iostream>
+        // #include <vector>
 
-using namespace std;
+        // using namespace std;
 
-int main() {
+        // int main() {
 
-    int size = 5;
-    int array[] = {1, 2, 3, 4, 5};
+        //     int size = 5;
+        //     int array[] = {1, 2, 3, 4, 5};
 
-    int maximumSum = 0;
+        //     int maximumSum = 0;
 
-    for ( int start = 0; start< size; start++ ) {
-        int currentSum = 0;
-        for ( int end= start; end< size; end++ ) {
-            currentSum += array[end];
-            maximumSum = max ( maximumSum, currentSum );
+        //     for ( int start = 0; start< size; start++ ) {
+        //         int currentSum = 0;
+        //         for ( int end= start; end< size; end++ ) {
+        //             currentSum += array[end];
+        //             maximumSum = max ( maximumSum, currentSum );
+        //         }
+        //     }
+
+        //     cout << "The Maximum Sub Array Sub Is : " << maximumSum << endl;
+
+        //     return 0;
+        // }
+
+
+
+// KADANE'S ALGORITHM ( MOST OPTIMISED ) ---->
+
+
+    // Kadane's algo says if one one number is negative then no need to add this number, insted of add 0 - so that sum won't negative.!
+
+    // Apply one loop and add serially number in currentSum if sum goes to negative then reset it into 0..  Because - it will decrease currentSum
+
+    // currentSUm = 0, 3, -1, 0, 5, 9, 8, 15, 7
+    // maximumSum = 0, 3, 5, 9, 15
+
+
+        #include <iostream>
+        using namespace std;
+
+
+        int main() {
+
+            int n = 7;
+            int array [] = { 3, -4, 5, 4, -1, 7, -8 };
+
+            int currentSum = 0, maximumSum = 0;
+
+            for ( int i= 0; i< n; i++ ) {
+                currentSum += array[i];
+                maximumSum = max ( maximumSum, currentSum );
+                if( currentSum < 0 ) {
+                    currentSum = 0;
+                }
+            }
+
+            cout << "The Maximum Sum Of Sub Arrays Is : " << maximumSum << endl;
+
+            return 0;
         }
-    }
 
-    cout << "The Maximum Sub Array Sub Is : " << maximumSum << endl;
 
-    return 0;
-}
